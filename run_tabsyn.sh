fcf96f0c75360e47145a1b252ac7f7f6f7bc3607
Benjamin Pöhlmann <benjamin_poehlmann@web.de>
2025-04-20 19:03:49 +0200

    tabsyn script

---
+++ run_tabsyn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd "$(dirname "$0")"          # /content/tabsyn
+# 2) expose your repo on PYTHONPATH
+export PYTHONPATH="$PWD"
+
+# 3) run the root entry‑point with the right flags
+python3 main.py \
+  --dataname tm_1804 \
+  --method tabsyn \
+  --mode train \
+  --gpu 0
+
