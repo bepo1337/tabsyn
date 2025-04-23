bbb754e7d05658584b3b06102721a8978e58227e
Benjamin Pöhlmann <benjamin_poehlmann@web.de>
2025-04-23 13:47:56 +0200

    add run_sample.sh

--- run_sample.sh
+++ run_sample.sh
@@ -5,6 +5,7 @@ export PYTHONPATH="$PWD"
 python3 main.py \
   --dataname tm_1804 \
   --method tabsyn \
-  --mode train \
+  --mode sample \
+  --save_path=tm_2304_samples.json \
   --gpu 0
 
--- run_vae.sh
+++ run_vae.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"          # /content/tabsyn
-# 2) expose your repo on PYTHONPATH
 export PYTHONPATH="$PWD"
 
-# 3) run the root entry‑point with the right flags
 python3 main.py \
   --dataname tm_1804 \
   --method vae \
